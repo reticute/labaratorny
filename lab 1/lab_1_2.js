5100a347e69e83da0297fd827b7f77d1d51be0fa
reticute <127994507+reticute@users.noreply.github.com>
2026-09-23 17:28:54 +0300

    added 'use strict'

--- lab 1/lab_1_2.js
+++ lab 1/lab_1_2.js
@@ -1,6 +1,9 @@
+'use strict'
+
 const array = [
     true, false, 67, 'hello world', 8.4, -12803, 'JavaScript', 'great', 909,
-    -0.15, 413, true, 'totally three words', 'gggg', 300000, 15.9
+    -0.15, 413, true, 'totally three words', 'gggg', 300000, 15.9,
+    '#34008D', '#f2a400',
 ]
 
 const empty = {number: 0, string: 0, boolean: 0}
