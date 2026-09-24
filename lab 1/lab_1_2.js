2fdaf7e1fb56b215e82a60b5ca1490c1bb1bf9bd
reticute <127994507+reticute@users.noreply.github.com>
2026-09-24 21:24:32 +0300

    a

--- lab 1/lab_1_2.js
+++ lab 1/lab_1_2.js
@@ -3,7 +3,7 @@
 const array = [
     true, false, 67, 'hello world', 8.4, -12803, 'JavaScript', 'great', 909,
     -0.15, 413, true, 'totally three words', 'gggg', 300000, 15.9,
-    '#34008D', '#f2a400',
+    '#34008D', '#f2a400',  null
 ]
 
 const empty = {number: 0, string: 0, boolean: 0}
@@ -12,20 +12,20 @@ const empty = {number: 0, string: 0, boolean: 0}
 for (const item of array) {
     const type = typeof item;
     
-   if (type in array) {
-    array[type]++
+   if (type in empty) {
+    empty[type]++
    }
 }
 
-console.dir(array, empty)
+console.dir(empty)
 
 const dynamicArray = {}
 for (const item of array) {
   const type = typeof item;
 
-  if (!(type in dynamicArray)) 
+  if (!(type in dynamicArray)) {
 	dynamicArray[type] = 0;
-
+  }
 
 dynamicArray[type]++
 
